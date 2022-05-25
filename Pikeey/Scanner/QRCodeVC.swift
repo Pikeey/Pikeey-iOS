0e6f22f092ab75cd6eeefbc27827e9ba8659b535
Eric <eric.morales@protonmail.com>
2022-05-24 23:49:45 -0500

    feat(qrScanner): add landing page and scanner

--- Pikeey/Scanner/QRCodeVC.swift
+++ Pikeey/Scanner/QRCodeVC.swift
@@ -9,13 +9,106 @@ import UIKit
 
 class QRCodeVC: UIViewController {
 
+    // MARK: - Properties
+    lazy var labelStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.alignment = .center
+        
+        return stack
+    }()
+    lazy var brandLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.text = "Picky"
+        
+        return label
+    }()
+    lazy var scanButton: UIButton = {
+        let button = UIButton(type: .roundedRect)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 8
+        button.layer.masksToBounds = true
+        button.backgroundColor = .secondarySystemBackground
+        button.setImage(UIImage(systemName: "qrcode.viewfinder"), for: .normal)
+        
+        // Title
+        let string = "   Scan QR Code"
+        let attributes: [NSAttributedString.Key : Any] = [
+            .font : UIFont.preferredFont(forTextStyle: .headline),
+            .foregroundColor : UIColor.label
+        ]
+        let attributeString = NSAttributedString(string: string, attributes: attributes)
+        button.setAttributedTitle(attributeString, for: .normal)
+        
+        button.addTarget(self, action: #selector(scanButtonSelected(_:)), for: .touchUpInside)
+
+
+        return button
+    }()
+    
+    // MARK: - VC's LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         view.backgroundColor = .systemBackground
+        setupNavBar()
+        setupBrandLabel()
+        setupScanButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // Make sure the navigationBar is always being display for this VC.
+        self.navigationController?.navigationBar.isHidden = false
     }
 
-
+    // MARK: - Methods
+    private func setupNavBar() {
+        let loginButton = UIBarButtonItem(title: "Log In", style: .plain, target: self, action: #selector(loginButtonSelected(_:)))
+        loginButton.setTitleTextAttributes([NSAttributedString.Key.underlineStyle : NSUnderlineStyle.single], for: .normal)
+        self.navigationItem.rightBarButtonItem = loginButton
+    }
+    
+    private func setupBrandLabel() {
+        // Add to view's hierarchy
+        view.addSubview(labelStackView)
+        labelStackView.addArrangedSubview(brandLabel)
+        
+        // Add constraints
+        NSLayoutConstraint.activate([
+            labelStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            labelStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            labelStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            labelStackView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.65)
+        ])
+    }
+    
+    private func setupScanButton() {
+        // Add to view's hierarchy
+        view.addSubview(scanButton)
+        
+        // Add constraints
+        NSLayoutConstraint.activate([
+            scanButton.topAnchor.constraint(equalTo: labelStackView.bottomAnchor, constant: 30),
+            scanButton.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.2),
+            scanButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.7),
+            scanButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+        ])
+    }
+    
+    @objc private func loginButtonSelected(_ button: UIBarButtonItem) {
+        print("Log In Button Selected.")
+    }
+    
+    @objc func scanButtonSelected(_ button: UIButton) {
+        print("Scan button tapped!")
+        
+        navigationController?.pushViewController(QRScannerVC(), animated: true)
+    }
 }
 
---
+++ Pikeey/Scanner/QRScannerVC.swift
@@ -0,0 +1,219 @@
+//
+//  QRScannerVC.swift
+//  Pikeey
+//
+//  Created by Eric Morales on 5/24/22.
+//
+
+import UIKit
+import AVFoundation
+
+class QRScannerVC: UIViewController {
+
+    // MARK: - Properties
+    lazy var captureSession = AVCaptureSession()
+    lazy var videoPreviewLayer: AVCaptureVideoPreviewLayer? = nil
+    lazy var qrCodeFrameView: UIView? = nil
+    lazy var promptStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.backgroundColor = .secondarySystemBackground
+        stack.axis = .horizontal
+        stack.distribution = .fill
+        stack.alignment = .center
+        stack.layer.cornerRadius = 8
+        stack.layer.shadowOpacity = 0.15
+        stack.layer.shadowOffset.height = 3
+        
+        
+        return stack
+    }()
+    lazy var promptLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.text = "Scan the code to see the menu."
+        label.numberOfLines = 2
+        
+        return label
+    }()
+    lazy var isLightOn: Bool = false {
+        didSet {
+            // Depending on this value change the image of the flashlight button property.
+            isLightOn ? flashLightButton.setImage(UIImage(systemName: "flashlight.on.fill"), for: .normal) : flashLightButton.setImage(UIImage(systemName: "flashlight.off.fill"), for: .normal)
+            
+            // Checking if user is on dark/light mode
+            if traitCollection.userInterfaceStyle == .dark {
+                // Dark mode
+                flashLightButton.backgroundColor = isLightOn ? .darkGray : .secondarySystemBackground
+            } else {
+                // Light mode
+                flashLightButton.backgroundColor = isLightOn ? .secondarySystemBackground : .darkGray
+            }
+            
+        }
+    }
+    lazy var flashLightButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "flashlight.off.fill"), for: .normal)
+        button.addTarget(self, action: #selector(toggleTorch(_:)), for: .touchUpInside)
+        
+        let backgroundColor: UIColor
+        if traitCollection.userInterfaceStyle == .dark { backgroundColor = .secondarySystemBackground } else { backgroundColor = .darkGray }
+        button.backgroundColor = backgroundColor
+        button.tintColor = .label
+        button.layer.cornerRadius = 20
+        button.layer.shadowOpacity = 0.15
+        button.layer.shadowOffset.height = 3
+        
+        return button
+    }()
+    
+    // MARK: - VC's LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        setupNavBar()
+        setupQRCodeScanner()
+        setupPrompt()
+        setupFlashLightButton()
+    }
+    
+    // MARK: - Methods
+    private func setupNavBar() {
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    private func setupPrompt() {
+        // Add to view's hierarchy
+        view.addSubview(promptStack)
+        promptStack.addArrangedSubview(promptLabel)
+        
+        // Add constraints
+        NSLayoutConstraint.activate([
+            promptStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            promptStack.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
+            promptStack.heightAnchor.constraint(equalTo: promptLabel.heightAnchor, constant: 20),
+            promptStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            promptLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            promptLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+    }
+    
+    private func setupFlashLightButton() {
+        // Add view's hierarchy
+        view.addSubview(flashLightButton)
+        
+        // Add constraints
+        NSLayoutConstraint.activate([
+            flashLightButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            flashLightButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            flashLightButton.heightAnchor.constraint(equalTo: promptStack.heightAnchor, multiplier: 1),
+            flashLightButton.widthAnchor.constraint(equalTo: flashLightButton.heightAnchor),
+        ])
+    }
+    
+    private func setupQRCodeScanner() {
+        // Get the back-facing camera for capturing videos
+        guard let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
+            print("Failed to get the camera device")
+            return
+        }
+        
+        do {
+            // Get an instance of the AVCaptureDeviceInput class using the previous device object
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            // Set the input device on the capture session
+            captureSession.addInput(input)
+            
+            // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession.addOutput(captureMetadataOutput)
+            
+            // Set delegate and use the default dispatch queue to execute the call back
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            
+            // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            
+            // Start video capture
+            captureSession.startRunning()
+            
+            // Initialize QR Code Frame to highlight the QR Code
+            qrCodeFrameView = UIView()
+            
+            if let qrcodeFrameView = qrCodeFrameView {
+                qrcodeFrameView.layer.borderColor = UIColor.systemYellow.cgColor
+                qrcodeFrameView.layer.borderWidth = 2
+                
+                view.addSubview(qrcodeFrameView)
+                view.bringSubviewToFront(qrcodeFrameView)
+            }
+            
+        } catch {
+            // If any error occurs, simply print it out and don't continue anymore
+            print(error)
+            return
+        }
+    }
+    
+    @objc private func toggleTorch(_ button: UIButton) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        
+        isLightOn.toggle()
+        
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+                
+                device.torchMode = isLightOn ? .on : .off
+                
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
+}
+
+extension QRScannerVC: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        // Check if the metadataObjects array is not nil and it contains at least one object
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            return
+        }
+        
+        // Get the metadata object
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == .qr {
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                
+                
+                // Once the QR Code is detected this code will run with the stringValue
+                print(metadataObj.stringValue!)
+                
+                
+                
+            }
+        }
+    }
+}
